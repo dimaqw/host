de1064b006c0cb99b5a520bce81f8c7de6800dcf
dimaqw <dimaqw@gmail.com>
2013-09-19 10:29:31 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,26 +1,19 @@
-alert(1);
-//alert(12); 
 var d = document;
-var cnv = d.getElementById('cnv');
 var diam = 5;
-
   
 function pp2(e){
-  
   var cnv = d.getElementById('cnv');
      x = e.pageX - cnv.offsetLeft;
      y = e.pageY - cnv.offsetTop;
      t('drw_ch(x,y)',4,100)
      //drw_ch(x,y);
      diam=5;
-     
 } 
    
    
 function drw_ch(xp,yp){ 
    var cnv = d.getElementById('cnv'); 
    var cntx = cnv.getContext('2d'); 
-    
     cntx.beginPath(); 
     cntx.arc(xp,yp,diam,0,Math.PI*2,false); 
     cntx.closePath(); 
@@ -36,19 +29,19 @@ var i=0;
   }
 }
   
-$('document').ready(function(){ 
- cnv.addEventListener('click',pp2,false); 
- var cntx = cnv.getContext('2d'); 
- for(var x=0;x<=540;x+=60){ 
+  
+$('document').ready(function(){
+  var cnv = d.getElementById('cnv');
+  cnv.addEventListener('click',pp2,false); 
+  var cntx = cnv.getContext('2d'); 
+  for(var x=0;x<=540;x+=60){ 
    cntx.moveTo(x,0); 
    cntx.lineTo(x,450); 
- } 
-      
- for(var y=0;y<=450;y+=50){ 
+  } 
+  for(var y=0;y<=450;y+=50){ 
    cntx.moveTo(0,y); 
    cntx.lineTo(540,y); 
- } 
-    cntx.strokeStyle = '#eee'; 
-    cntx.stroke(); 
-      
+  } 
+  cntx.strokeStyle = '#eee'; 
+  cntx.stroke(); 
 }); 
