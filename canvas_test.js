3dd6553cad2d7e89d4d7c931f20e149540a2faa4
dimaqw <dimaqw@gmail.com>
2013-09-23 09:49:23 +0300

    Create canvas_test.js

---
+++ canvas_test.js
@@ -0,0 +1,7 @@
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+var img=document.getElementById("lamp");
+var pat=ctx.createPattern(img,"repeat");
+ctx.rect(0,0,150,100);
+ctx.fillStyle=pat;
+ctx.fill();
