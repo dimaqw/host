19ac6e701139670d293128808642110d6102770d
dimaqw <dimaqw@gmail.com>
2015-05-23 23:34:18 +0300

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,6 +1,6 @@
 <title>Функции</title>
 <?php
-
+header('Content-Type: text/html; charset=utf-8');
 function linkk($linkk,$title)
 {
    echo "<a href='$linkk'>$title</a>";
@@ -12,30 +12,34 @@ echo "<br>";
 function square($a)
 {
    $result=$a*$a;
-   echo $result;
+   return $result;
 }
-square(2);
+echo square(2);
+echo "<br>";
 
 function plus ($b,$c)
 {
    $result1 = $b+$c;
-   echo "<br>";
-   echo $result1;
+   
+   return $result1;
 }
-plus (9,2);
+echo plus (9,2);
+echo "<br>";
 
 function sqrt1 ($d)
 {
    $result2 = sqrt($d);
-   echo "<br>";
-   echo $result2;
+ 
+   return $result2;
 }
-sqrt1 (16);
+echo sqrt1 (16);
+echo "<br>";
 
 function sum ($e,$f,$g)
 {
    $result3 = $e+$f+$g;
-   echo "<br>";
-   echo $result3;
+   
+   return $result3;
+
 }
-sum (1,2,3);
+echo sum (square(2),plus (9,2),sqrt1 (16));
