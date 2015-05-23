2ac1601c5578999ada2a030143703d6320be3124
dimaqw <dimaqw@gmail.com>
2015-05-23 23:37:28 +0300

    Update test2.php

--- test2.php
+++ test2.php
@@ -14,7 +14,7 @@ function square($a)
    $result=$a*$a;
    return $result;
 }
-echo square(2);
+echo $e = square(2);
 echo "<br>";
 
 function plus ($b,$c)
@@ -23,16 +23,16 @@ function plus ($b,$c)
    
    return $result1;
 }
-echo plus (9,2);
+echo $f = plus (9,2);
 echo "<br>";
 
 function sqrt1 ($d)
 {
    $result2 = sqrt($d);
- 
+  
    return $result2;
 }
-echo sqrt1 (16);
+echo $g = sqrt1 (16);
 echo "<br>";
 
 function sum ($e,$f,$g)
@@ -42,4 +42,4 @@ function sum ($e,$f,$g)
    return $result3;
 
 }
-echo sum (square(2),plus (9,2),sqrt1 (16));
+echo sum ($e,$f,$g);
